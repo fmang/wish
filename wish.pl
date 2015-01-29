9ad245f198a25f849778cb83d5ccfe37cffd2f16
Frédéric Mangano <fmang@mg0.fr>
2015-01-29 20:16:11 +0100

    use Dancer to provide a web interface

--- wish.pl
+++ wish.pl
@@ -1,6 +1,18 @@
+#!/usr/bin/perl
+
+use Dancer;
+use Data::Dumper;
 use Wish::KanjiDic;
 
 binmode(STDOUT, ":utf8");
 
 my $w = Wish::KanjiDic->new();
 $w->load('data/kanjidic');
+
+get '/kanji/:query' => sub {
+	my %kanji = $w->lookup(param('query'));
+	return Dumper(\%kanji) if %kanji;
+	return 'Unknown kanji';
+};
+
+dance;
